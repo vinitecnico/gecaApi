3694019a7b26b6b69498f5ae84eb02e9faa3cc57
Paulo Jean <paulo_jean25@hotmail.com>
2018-12-17 19:01:02 -0200

    Update config.js

--- conf/config.js
+++ conf/config.js
@@ -1,6 +1,6 @@
   module.exports = {
     //mongoURI: "mongodb://127.0.0.1:27017/",
-    ongoURI: "mongodb://gecbase:6LYjp5377d4bVjc@ds137812.mlab.com:37812/baseinit",
+    mongoURI: "mongodb://gecbase:6LYjp5377d4bVjc@ds137812.mlab.com:37812/baseinit",
     hostname: "localhost" ,
     port: 3000        
   };
