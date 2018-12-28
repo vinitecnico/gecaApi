9d774aa44b6bde66a704dae1ea7212234dbd0e7e
Vinicius Rodrigues Santos <vrodrisa@everis.com>
2018-12-28 14:40:03 -0200

    fix

--- controller/feira.js
+++ controller/feira.js
@@ -242,81 +242,84 @@ exports.deleteFeira = (request, response, next) => {
 
 ///POST import database
 exports.postImportDatabase = async (request, response, next) => {
+    try {
+        MongoClient.connect(require("../conf/config").mongoURI, { useNewUrlParser: true }, function (erro, db) {
 
-    MongoClient.connect(require("../conf/config").mongoURI, { useNewUrlParser: true }, function (erro, db) {
+            if (erro) {
 
-        if (erro) {
+                response.status(status.BAD_REQUEST).send(JSON.stringify(erro));
 
-            response.status(status.BAD_REQUEST).send(JSON.stringify(erro));
+            } else {
+                /// DataBase
+                var dbo = db.db("baseinit");
+                var StringDecoder = require('string_decoder').StringDecoder;
+                var d = new StringDecoder('utf8');
+                var promises = [];
 
-        } else {
-            /// DataBase
-            var dbo = db.db("baseinit");
-            var StringDecoder = require('string_decoder').StringDecoder;
-            var d = new StringDecoder('utf8');
-            var promises = [];
+                const tb_feiras = require('../dbFile/tb_feiras.json');
 
-            const tb_feiras = require('../dbFile/tb_feiras.json');
+                for (var i = 0; i < tb_feiras.length; i++) {
+                    ///Verifa se cpf ja existe na base
 
-            for (var i = 0; i < tb_feiras.length; i++) {
-                ///Verifa se cpf ja existe na base
+                    if (tb_feiras[i].chr_dia && tb_feiras[i].chr_cep && tb_feiras[i].chr_nome) {
 
-                if (tb_feiras[i].chr_dia && tb_feiras[i].chr_cep && tb_feiras[i].chr_nome) {
+                        promises.push(dbo.collection("feiras").find({ "zipcode": tb_feiras[i].chr_cep })
+                            .toArray((err, res) => {
+                                if (err) {
+                                    console.log(err);
+                                } else {
 
-                    promises.push(dbo.collection("feiras").find({ "zipcode": tb_feiras[i].chr_cep })
-                        .toArray(function (err, res) {
-                            if (err) {
-                                console.log(err);
-                            } else {
+                                    if (res.length == 0) {
+                                        ///Object para inserção
+                                        var myobj = {
+                                            "name": d.write(tb_feiras[i].chr_nome),
+                                            "weekday": tb_feiras[i].chr_dia,
+                                            "zipcode": tb_feiras[i].chr_cep.replace('-', ''),
+                                            "address": tb_feiras[i].chr_rua ? d.write(tb_feiras[i].chr_rua) : null,
+                                            "numberAddress": tb_feiras[i].chr_numero,
+                                            "complement": tb_feiras[i].chr_complemento ? d.write(tb_feiras[i].chr_complemento) : null,
+                                            "neighborhood": tb_feiras[i].chr_bairro,
+                                            "city": tb_feiras[i].chr_cidade ? d.write(tb_feiras[i].chr_cidade) : null,
+                                            "state": tb_feiras[i].chr_estado,
+                                            "gps": tb_feiras[i].chr_gps,
+                                            "datacreate": new Date(Date.now()),
+                                            "dataUpdate": new Date(Date.now())
+                                        }
 
-                                if (res.length == 0) {
-                                    ///Object para inserção
-                                    var myobj = {
-                                        "name": d.write(tb_feiras[i].chr_nome),
-                                        "weekday": tb_feiras[i].chr_dia,
-                                        "zipcode": tb_feiras[i].chr_cep.replace('-', ''),
-                                        "address": tb_feiras[i].chr_rua ? d.write(tb_feiras[i].chr_rua) : null,
-                                        "numberAddress": tb_feiras[i].chr_numero,
-                                        "complement": tb_feiras[i].chr_complemento ? d.write(tb_feiras[i].chr_complemento) : null,
-                                        "neighborhood": tb_feiras[i].chr_bairro,
-                                        "city": tb_feiras[i].chr_cidade ? d.write(tb_feiras[i].chr_cidade) : null,
-                                        "state": tb_feiras[i].chr_estado,
-                                        "gps": tb_feiras[i].chr_gps,
-                                        "datacreate": new Date(Date.now()),
-                                        "dataUpdate": new Date(Date.now())
+                                        return dbo.collection("feiras").insertOne(myobj, (err, res) => {
+
+                                            if (err) {
+                                                // response.status(status.BAD_REQUEST).send(JSON.stringify(err));
+                                                console.log(err);
+                                            }
+                                            else {
+
+                                                // response.status(status.OK).send(JSON.stringify("Feira cadastrada com sucesso"));
+                                                console.log("Feira cadastrada com sucesso");
+                                            }
+
+                                            db.close();
+                                            const defer = Q.defer();
+                                            return defer.promise;
+
+                                        });
                                     }
 
-                                    return dbo.collection("feiras").insertOne(myobj, function (err, res) {
-
-                                        if (err) {
-                                            // response.status(status.BAD_REQUEST).send(JSON.stringify(err));
-                                            console.log(err);
-                                        }
-                                        else {
-
-                                            // response.status(status.OK).send(JSON.stringify("Feira cadastrada com sucesso"));
-                                            console.log("Feira cadastrada com sucesso");
-                                        }
-
-                                        db.close();
-                                        const defer = Q.defer();
-                                        return defer.promise;
-
-                                    });
                                 }
+                                db.close();
+                            }));
+                    }
 
-                            }
-                            db.close();
-                        }));
                 }
-
+                Q.all(promises)
+                    .then(() => {
+                        response.status(status.OK).send(JSON.stringify("Feira cadastrada com sucesso"));
+                    });
             }
-            Q.all(promises)
-                .then(() => {
-                    response.status(status.OK).send(JSON.stringify("Feira cadastrada com sucesso"));
-                });
-        }
 
-    });
-
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
 }
