3cf5de6d1945d5e8b737f28faae4795c9012272a
Paulo Jean <paulo_jean25@hotmail.com>
2019-01-07 21:54:02 -0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials' , true);
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization, x-access-token');
+    res.setHeader('Content-Type', 'application/json');
     next();
 });
 
