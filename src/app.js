1fe470e8a403b1cafb7d5e4fa319c38f7c1f993c
Paulo Jean <paulo_jean25@hotmail.com>
2019-01-07 20:50:51 -0200

    Update app.js
    Change a x-access-token 4 authorization

--- src/app.js
+++ src/app.js
@@ -26,7 +26,8 @@ app.use(express.json())
 app.use(function (req, res, next) {
 
     if (req.originalUrl != "/api/login") {
-        var token = req.headers['x-access-token'];
+        //var token = req.headers['x-access-token'];
+        var token = req.headers.authorization.replace('Bearer ' , '')
 
         if (token) {
             jwt.verify(token, require("../conf/config").configName, function (err, decoded) {
