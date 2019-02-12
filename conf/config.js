4ef388cd7869c78ecf158d01e1469a7e2926bdc9
Vinicius Rodrigues Santos <vrodrisa@everis.com>
2019-02-12 09:58:09 -0200

    fix upload file

--- conf/config.js
+++ conf/config.js
@@ -1,17 +1,15 @@
-  module.exports = {
-    mongoURI: "mongodb://127.0.0.1:27017/",
-    //mongoURI: "mongodb://gecbase:6LYjp5377d4bVjc@ds137812.mlab.com:37812/baseinit",
-    hostname: "localhost" ,
-    port: 3000 , 
-    host: 'smtp.gmail.com',
-    porthost: 587,
-    secure: false,
-    usermail: "gecaenvio@gmail.com",
-    passmail: "geca2018" ,
-    keyGoogleMaps: 'AIzaSyAdyvRIRT1iMFRzE4v8BzfGXc-Oc9OFTwk',    
-    configName: 'g3c4T0k3n#!', 
-    expireInTime: 10800 ,
-    xrapidapikey: "e8c62da454mshe004396abdaf993p1d2c7ejsn012115a2fc24",
-    subdomain: "agencialeak" 
-
-  };
+module.exports = {
+  //mongoURI: "mongodb://127.0.0.1:27017/",
+  mongoURI: "mongodb://gecbase:6LYjp5377d4bVjc@ds137812.mlab.com:37812/baseinit",
+  hostname: "localhost" ,
+  port: 3000 , 
+  host: 'smtp.gmail.com',
+  porthost: 587,
+  secure: false,
+  usermail: "gecaenvio@gmail.com",
+  passmail: "geca2018" ,
+  keyGoogleMaps: 'AIzaSyAdyvRIRT1iMFRzE4v8BzfGXc-Oc9OFTwk',    
+  configName: 'g3c4T0k3n#!', 
+  expireInTime: 10800 ,
+  keyDropbox: '1TDzKQVLbQAAAAAAAAAAEKY7QIlklzKVIz5OxGjjLJbB0JmilBhROQdakOGKeeZu' 
+};
--- controller/login.js
+++ controller/login.js
@@ -1,59 +1,59 @@
-var MongoClient = require('mongodb').MongoClient;
-const status    = require('http-status');
-var bcrypt      = require('bcryptjs');
-var jwt         = require('jsonwebtoken');
+const MongoClient = require('mongodb').MongoClient;
+const status = require('http-status');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-var obj = {};
+let obj = {};
 
 exports.postLogin = (request, response, next) => {
 
     MongoClient.connect(require("../conf/config").mongoURI, { useNewUrlParser: true }, function (erro, db) {
 
-        if (erro)
+        if (erro) {
             response.status(status.BAD_REQUEST).send(JSON.stringify(erro));
-
-        db.db("baseinit").collection("users").findOne({ email: request.body.email }, function (err, res) {
-            if (err)
-                response.status(status.BAD_REQUEST).send(JSON.stringify(err));
-            if (!res) {
-
-                obj = {
-                    sucess : false,
-                    status : status.UNAUTHORIZED ,
-                    messsage : 'Autenticação do Usuário falhou. Usuário não encontrado!',
-                    token: "Não há token."
-                }
-
-                response.status(status.UNAUTHORIZED).send(obj);                
-
-            } else if (res) {
-
-                if(!bcrypt.compareSync(request.body.password, res.password)) {
+        } else {
+            db.db("baseinit").collection("users").findOne({ email: request.body.email }, function (err, res) {
+                if (err)
+                    response.status(status.BAD_REQUEST).send(JSON.stringify(err));
+                if (!res) {
 
                     obj = {
-                        sucess : false,
-                        status : status.UNAUTHORIZED,                        
-                        messsage: 'Autenticação do Usuário falhou. Senha incorreta!',
+                        sucess: false,
+                        status: status.UNAUTHORIZED,
+                        messsage: 'Autenticação do Usuário falhou. Usuário não encontrado!',
                         token: "Não há token."
                     }
 
-                    response.status(status.UNAUTHORIZED).send(obj);                  
-                }
-                else{                                        
+                    response.status(status.UNAUTHORIZED).send(obj);
 
-                    obj = {
-                        sucess : true,
-                        status : status.OK, 
-                        name : res.name ,
-                        messsage: 'Usuário Autenticado com Sucesso!', 
-                        token : jwt.sign(res, require("../conf/config").configName, {expiresIn: require("../conf/config").expireInTime })
+                } else if (res) {
+
+                    if (!bcrypt.compareSync(request.body.password, res.password)) {
+
+                        obj = {
+                            sucess: false,
+                            status: status.UNAUTHORIZED,
+                            messsage: 'Autenticação do Usuário falhou. Senha incorreta!',
+                            token: "Não há token."
+                        }
+
+                        response.status(status.UNAUTHORIZED).send(obj);
                     }
+                    else {
 
-                    response.status(status.OK).send(obj);
+                        obj = {
+                            sucess: true,
+                            status: status.OK,
+                            name: res.name,
+                            messsage: 'Usuário Autenticado com Sucesso!',
+                            token: jwt.sign(res, require("../conf/config").configName, { expiresIn: require("../conf/config").expireInTime })
+                        }
+
+                        response.status(status.OK).send(obj);
+                    }
                 }
-            }
-
-        });
+            });
+        }
     });
 
 }
