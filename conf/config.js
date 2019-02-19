5fae0c629d3638a0687dac6218bd02be8ee0a7d8
Paulo Jean <paulo_jean25@hotmail.com>
2019-02-18 23:35:48 -0300

    Update Config email

--- conf/config.js
+++ conf/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // mongoURI: "mongodb://127.0.0.1:27017/",
+  //mongoURI: "mongodb://127.0.0.1:27017/",
   mongoURI: "mongodb://gecbase:6LYjp5377d4bVjc@ds137812.mlab.com:37812/baseinit",
   hostname: "localhost" ,
   port: 3000 , 
--- controller/email.js
+++ controller/email.js
@@ -4,65 +4,109 @@ const status = require('http-status');
 const conf = require("../conf/config");
 const Q = require('q');
 const _ = require("lodash");
+const request = require('request');
+
+function CreateListMaile(titulo) {
+    const defer = Q.defer();
+    const clientServerOptions = {
+        uri: require("../conf/config").urlMailee + 'lists',
+        qs: { api_key: require("../conf/config").xrapidapikey, subdomain: require("../conf/config").subdomain },
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': require("../conf/config").tokenxrapidapikey
+        },
+        form: { site: "agencialeak" , name : titulo  , phone: "(211)336-1440" }
+    };
+
+    try {
+        request(clientServerOptions, (error, response, body) => {
+            if (error) {
+                defer.resolve(null);
+            } else {
+                defer.resolve(JSON.parse(body).id);
+            }
+        });
+    } catch (error) {
+        defer.resolve(null);
+    }
+
+    return defer.promise;
+}
 
 exports.sendEmail = (request, response, next) => {
-    MongoClient.connect(conf.mongoURI, { useNewUrlParser: true }, function (erro, db) {
-        nodemailer.createTestAccount((err, account) => {
-            let transporter = nodemailer.createTransport({
-                host: conf.host,
-                port: conf.porthost,
-                secure: conf.secure,
-                auth: {
-                    user: conf.usermail,
-                    pass: conf.passmail
-                }
-            });
+    
+    var genre = request.body.criterion.genre;
+    if(genre.allProcess == true){
+        MongoClient.connect(conf.mongoURI, { useNewUrlParser: true }, function (erro, db) {
 
-            db.db("baseinit").collection("pessoa").find({})
-                .toArray(function (err, res) {
-                    if (err) {
-                        response.status(status.BAD_REQUEST).send(JSON.stringify(err));
-                    }
-                    else {
-                        if (res.length != 0) {
+            db.db("baseinit").collection("pessoa").find({"notificacoes_anotacoes.email": true})
+                 .toArray(function (err, res) {
+                    CreateListMaile(request.body.subject).then(data => {
+                        console.log(data);
+                    });
 
-                            res.forEach(function (doc) {
-                                if (doc.notificacoes_anotacoes.email) {
+                    
+                 })
+        })
+    }
+    // MongoClient.connect(conf.mongoURI, { useNewUrlParser: true }, function (erro, db) {
+    //     nodemailer.createTestAccount((err, account) => {
+    //         let transporter = nodemailer.createTransport({
+    //             host: conf.host,
+    //             port: conf.porthost,
+    //             secure: conf.secure,
+    //             auth: {
+    //                 user: conf.usermail,
+    //                 pass: conf.passmail
+    //             }
+    //         });
 
-                                    //setup email data with unicode symbols
-                                    let mailOptions = {
-                                        from: 'Geca email', // sender address
-                                        to: doc.endereco_contato.email, // list of receivers
-                                        subject: request.body.title, // Subject line
-                                        //text: 'Hello world?', // plain text body
-                                        html: request.body.text // html body
-                                    };
+    //         db.db("baseinit").collection("pessoa").find({})
+    //             .toArray(function (err, res) {
+    //                 if (err) {
+    //                     response.status(status.BAD_REQUEST).send(JSON.stringify(err));
+    //                 }
+    //                 else {
+    //                     if (res.length != 0) {
 
-                                    // send mail with defined transport object
-                                    transporter.sendMail(mailOptions, (error, info) => {
-                                        if (error) {
-                                            console.log('Email:', doc.endereco_contato.email);
-                                            return console.log(error);
-                                        }
-                                        console.log('Message sent: %s', doc.endereco_contato.email);
-                                        console.log('Message sent: %s', info.messageId);
-                                        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-                                    });
+    //                         res.forEach(function (doc) {
+    //                             if (doc.notificacoes_anotacoes.email) {
 
-                                }
-                                //console.log(doc);
-                            });
+    //                                 //setup email data with unicode symbols
+    //                                 let mailOptions = {
+    //                                     from: 'Geca email', // sender address
+    //                                     to: doc.endereco_contato.email, // list of receivers
+    //                                     subject: request.body.title, // Subject line
+    //                                     //text: 'Hello world?', // plain text body
+    //                                     html: request.body.text // html body
+    //                                 };
 
-                            response.status(status.OK).send(JSON.stringify("Email's Enviado(s) com sucesso."));
-                        } else {
-                            response.status(status.NOT_FOUND).send(JSON.stringify("Problemas ao envio de email"));
-                        }
-                    }
-                    db.close();
-                });
-        });
+    //                                 // send mail with defined transport object
+    //                                 transporter.sendMail(mailOptions, (error, info) => {
+    //                                     if (error) {
+    //                                         console.log('Email:', doc.endereco_contato.email);
+    //                                         return console.log(error);
+    //                                     }
+    //                                     console.log('Message sent: %s', doc.endereco_contato.email);
+    //                                     console.log('Message sent: %s', info.messageId);
+    //                                     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    //                                 });
 
-    });
+    //                             }
+    //                             //console.log(doc);
+    //                         });
+
+    //                         response.status(status.OK).send(JSON.stringify("Email's Enviado(s) com sucesso."));
+    //                     } else {
+    //                         response.status(status.NOT_FOUND).send(JSON.stringify("Problemas ao envio de email"));
+    //                     }
+    //                 }
+    //                 db.close();
+    //             });
+    //     });
+
+    // });
 
 }
 
@@ -77,7 +121,7 @@ const sortPessoa = (type) => {
     }
 }
 
-const filterType = (query) => {
+const filterType = () => {
     let filter = [];
     const itemFilter = {};
     itemFilter['endereco_contato.email'] = { '$exists': true, $ne: null, $ne: '' };
